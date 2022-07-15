1babc4ec3af35abb3c96791d2cff571ad0e13c8c
Augustus Thomas <33500951+augustus-thomas@users.noreply.github.com>
2022-07-15 10:50:22 -0500

    Outlined algorithm

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,32 @@
-def arithmetic_arranger(problems):
+import re
+import sys
 
 
-    return arranged_problems
+
+
+def arithmetic_arranger(problems, bool):
+    # Initializing variables
+    n = len(problems)
+    answer=""
+    fail_message=""
+    operations = {}
+
+    # Trimming input data
+
+
+    ## Transfering problems string to a workable operations dictionary
+    ## Note: this step could be combined with trimming for computational efficieny, but is done seperately for readability.
+
+
+
+    # Checking and passing  provisioned errors
+
+
+    # Computing intermediary values
+
+
+    # Arranging Regex
+
+
+    # Outputing Regex and error message
+    return answer, fail_message
