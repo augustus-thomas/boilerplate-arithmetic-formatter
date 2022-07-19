9b4d13feeb0a4fed320ae46acd476aeb91e02d18
Augustus Thomas <33500951+augustus-thomas@users.noreply.github.com>
2022-07-19 15:23:06 -0500

    fix: regex for operators and operands

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from pytest import main
 
 from arithmetic_arranger import arithmetic_arranger
 
-print("imports complete")
-
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 # Run unit tests automatically
 
-##print("running unit tests")
-##main(['-vv'])
+main(['-vv'])
