d20e3813495f21b03885c22be0f2cf4a0432a676
Augustus Thomas <33500951+augustus-thomas@users.noreply.github.com>
2022-07-19 14:44:54 -0500

    added .gitignore for env

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -4,67 +4,63 @@ import re
 import sys
 from xmlrpc.client import boolean
 
-
-
-
-def arithmetic_arranger(problems, isSolved):
+def arithmetic_arranger(problems, *isSolved):
     # Initializing variables
     n = len(problems)
     answer=""
     fail_message=""
-    operations = {}
+    operations = {5}
     
-    # Trimming input data
-    operators = []
-    operands = []
-    for x in problems: 
-        new_operator = re.findall('\s(\S+)\s', x)
-        new_operand = re.findall('(*)\s\S\s(*)', x)
-        operators.append(new_operator)
-        operands.append(new_operand)
+    if __name__ == "__main__":
+        operators = []
+        operands = []
+        for x in problems: 
+            new_operator = re.findall('\w(\W+)\w', x)
+            new_operand = re.findall('(\W+)\w\W\w(\W+)', x)
+            operators.append(new_operator)
+            operands.append(new_operand)
+        print(operators)
+        print(operands)
+        # Extracting operands lists into one list
+        decompressed_operands = []
+        i = 0
+        while i < len(operands):
+            j = 0
+            while j < len(operands[i]):
+                decompressed_operands.append(operands[i][j])
+        print(decompressed_operands)
 
-    ## Note: Operands becomes a list of lists (tuple?) we need to handle this
-    decompressed_operands = []
-    i = 0
-    while i < len(operands):
-        j = 0
-        while j < len(operands[i]):
-            decompressed_operands.append(operands[i][j])
+        ## Transfering problems string to a workable operations dictionary
+        ## Note: this step could be combined with trimming for computational efficieny, but
+        ## is done seperately for readability.
 
 
+        # Checking and passing  provisioned errors
+        if (n > 5):
+            return ("Error: Too many problems.")
+        
+        for x in operators:
+            if ((x != "-") and (x != "+")):
+                return ("Error: Operator must be '+' or '-'.")
 
-    ## Transfering problems string to a workable operations dictionary
-    ## Note: this step could be combined with trimming for computational efficieny, but
-    ## is done seperately for readability.
+        
+        for y in decompressed_operands:
+            if (not isdigit(y)):
+                return ("Error: Numbers must only contain digits.")
 
-
-
-    # Checking and passing  provisioned errors
-    if n > 5:
-        raise Exception("Error: Too many problems.")
-    
-    for x in range(operators):
-        if (x != "-") and (x != "+"):
-            raise Exception("Error: Operator must be '+' or '-'.")
-            break
-    
-    for y in range(operands):
-        if not isdigit(y):
-            raise Exception("Error: Numbers must only contain digits.")
-            break
-
-        for y in range(operands):
+        for y in decompressed_operands:
             if abs(int(y)) >= 10000:
-                raise Exception("Error: Numbers cannot be more than four digits.")
-                break
-
-    
-    
-    # Computing intermediary values
+                return ("Error: Numbers cannot be more than four digits.")
 
 
-    # Arranging Regex
+        
+        
+        # Computing intermediary values
 
 
-    # Outputing Regex and error message
-    return answer, fail_message
+        # Arranging Regex
+
+
+        # Outputing Regex and error message
+        return operators
+    return operators
